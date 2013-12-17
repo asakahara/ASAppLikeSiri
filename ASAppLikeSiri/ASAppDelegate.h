42e45855316eb7675cbf8314e6ed84f9d8df0591
sakahara <sakahara@sakahara-no-MacBook-Pro.local>
2013-12-17 21:46:37 +0900

    Initial Commit

---
+++ ASAppLikeSiri/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  ASAppLikeSiri
+//
+//  Created by sakahara on 2013/12/17.
+//  Copyright (c) 2013年 Mocology. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
